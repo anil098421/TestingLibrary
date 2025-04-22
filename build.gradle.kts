0e663bd32f8ffe71ff974bd172494651664c6106
Anil Kumar <you@example.com>
2025-04-22 18:11:11 +0530

    Fix build files to resolve publishing conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("maven-publish")
 }
 
-// This is a workaround for JitPack
-val publishCommand = "publishToMavenLocal"
-val capitalized = publishCommand.replaceFirstChar { it.uppercase() }
-tasks.register(publishCommand) {
-    group = "publishing"
-    description = "Publishes all Maven publications to Maven Local"
-    dependsOn(gradle.includedBuilds.map { it.task(":$publishCommand") })
-}
-
-allprojects {
-    // Avoid issues with JitPack
-    group = "com.github.anil098421"
-}
+// Do not add publishToMavenLocal task at the root level
